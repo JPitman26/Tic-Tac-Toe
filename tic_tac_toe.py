bdd39cde027e7c0da3fedf12276d1d9badc7ccb1
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-08-01 22:24:43 +0100

    Gets the input (row and column) from the player.
    This function sets the row and col variables to 0 for 'X' player, and for 'O' player, it generates random row and column values to simulate the CPU's move.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -56,3 +56,24 @@ def is_board_full(board):
                 return False
     return True
 
+def get_input(board, player, row, col):
+    """
+    Gets the input (row and column) from the player.
+
+    Args:
+        board (list): A 2D list representing the Tic-Tac-Toe board.
+        player (str): The player's symbol ('X' or 'O').
+        row (tk.IntVar): Variable to store the chosen row.
+        col (tk.IntVar): Variable to store the chosen column.
+
+    Notes:
+        This function sets the row and col variables to 0 for 'X' player, 
+        and for 'O' player, it generates random row and column values 
+        to simulate the CPU's move. The actual CPU move is done in the 
+        cpu_move function.
+    """
+    if player == "X":
+        row.set(0)
+        col.set(0)
+    # No need for CPU move in this function
+
