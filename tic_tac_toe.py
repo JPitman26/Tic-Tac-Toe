6eedcb96f1c290621d4dee296aa25f64aaff7d12
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-08-01 22:22:54 +0100

    Update tic_tac_toe.py
    Prints the Tic-Tac-Toe board on the console.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,15 @@
 import random
 import tkinter as tk
 from tkinter import messagebox
+
+def print_board(board):
+    """
+    Prints the Tic-Tac-Toe board on the console.
+
+    Args:
+        board (list): A 2D list representing the Tic-Tac-Toe board.
+    """
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
