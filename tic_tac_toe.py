24d209974ba5481296f45b7acf510d7c807388b5
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-08-01 22:23:23 +0100

    Update tic_tac_toe.py
    Checks if a player has won the game.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -13,3 +13,30 @@ def print_board(board):
         print(" | ".join(row))
         print("-" * 9)
 
+def check_winner(board, player):
+    """
+    Checks if a player has won the game.
+
+    Args:
+        board (list): A 2D list representing the Tic-Tac-Toe board.
+        player (str): The player's symbol ('X' or 'O').
+
+    Returns:
+        bool: True if the player has won, False otherwise.
+    """
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)):
+        return True
+
+    if all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
